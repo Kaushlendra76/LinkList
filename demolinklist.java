234b708f8716b668799ba1216c1ec4441b209a2b
Kaushlendra Tiwari <kaushlendratiwari7607523802@gmail.com>
2022-10-08 15:34:43 +0530

    Add files via upload
    this is master main class file of linklist structure

---
+++ demolinklist.java
@@ -0,0 +1,18 @@
+/**
+ * demolinklist
+ */
+public class demolinklist {
+
+    public static void main(String[] args) {
+        JLinklist <Integer> list = new JLinklist<Integer>();
+        list.insertinfont(9);
+        list.printlist();
+        list.insertinfont(7);
+        list.printlist();
+        list.insertend(101);
+     //   list.insertkey(65, 9);
+       // list.printlist();
+
+
+    }
+}
